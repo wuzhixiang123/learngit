826bcc27bcc6a51474493fa90ab250f06f90526a
wuzhixiang123 <1192767445@qq.com>
2023-02-16 16:07:55 +0800

    2.16

---
+++ c++/test2.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+map<int, int> mp;
+int main()
+{
+ int n,a,b,ix,val;
+    cin>>n>>a>>b;
+    for(int i = 0;i < a;i++){
+        cin>>ix>>val;
+        if(ix >= n){
+           cout<<"error"<<endl;
+        }
+        mp[ix] = val;
+    }
+    int sum = 0;
+    for(int i = 0;i<b;i++){
+        cin>>ix>>val;
+        sum+=val*mp[ix];
+    }
+    cout<<sum<<endl;
+    return 0;
+}
